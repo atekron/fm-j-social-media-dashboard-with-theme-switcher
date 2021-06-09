305dbfb0c9e9c7e3af3471841b074e5e554703fd
Michael Kret <dirtysculptor@gmail.com>
2021-06-09 20:19:25 +0300

    mobile design and functionality done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+document.querySelector(".theme__toggle-bg").addEventListener("click", () => {
+  document.querySelector("body").classList.toggle("dark-mode");
+  let label = document.querySelector(".theme__label");
+  if (label.innerText === "Light Mode") {
+    label.innerText = "Dark Mode";
+  } else {
+    label.innerText = "Light Mode";
+  }
+});
